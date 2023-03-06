3b2ba51fc9eece234c6bdc04ef08503f5a385bb9
LeeGaHeee <gahui0415@gmail.com>
2023-03-06 21:35:49 +0900

    Create Components

---
+++ src/components/Footer.vue
@@ -0,0 +1,37 @@
+<template>
+  <footer>
+    <Logo></Logo>
+    <a href="" target="_black">
+      (c) {{ new Date().getFullYear() }} GAHEE
+    </a>
+  </footer>
+</template>
+
+<script>
+import Logo from './Logo.vue'
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../scss/main.scss";
+
+footer {
+  padding: 70px 0;
+  text-align: center;
+  opacity: .3;
+
+  .logo {
+    display: block;
+    margin-bottom: 4px; 
+    text-decoration: none;
+  }
+  a{
+    text-decoration: none;
+  }
+  
+}
+</style>
